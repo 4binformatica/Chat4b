d4d42ffa7e0d86418d2565d55f6f2e2d9ccf4bd3
Frederick67 <76555880+Frederick67@users.noreply.github.com>
2023-01-18 18:51:42 +0100

    Some azure fix

--- server/src/main/java/com/chat4b/App.java
+++ server/src/main/java/com/chat4b/App.java
@@ -5,6 +5,15 @@ import java.sql.SQLException;
 
 import org.java_websocket.server.WebSocketServer;
 
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
 public class App {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		String host = "localhost";
@@ -13,4 +22,25 @@ public class App {
 		WebSocketServer server = new Server(new InetSocketAddress(host, port));
 		server.run();
 	}
+
+	@FunctionName("httpExample")
+	public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        // Parse query parameter
+        final String query = request.getQueryParameters().get("name");
+        final String name = request.getBody().orElse(query);
+
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        }
+    }
 }
